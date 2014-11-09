554c2d7cf3ffef10a07c46ee54a1ffe40955cc9e
Nikita Titov <nmtitov@yandex.ru>
2014-11-10 01:22:24 +0200

    Remove deleted template context

--- nikitosios/settings.py
+++ nikitosios/settings.py
@@ -97,7 +97,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.static',
     'django.core.context_processors.media',
-    ''
+    'django.core.context_processors.request'
 ]
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
