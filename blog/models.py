5fa14f899efd788d459fbc58b501d9189da97e72
Nikita Titov <nmtitov@yandex.ru>
2015-01-05 22:23:33 +0200

    Ignore all *.sqlite3

--- blog/models.py
+++ blog/models.py
@@ -8,31 +8,13 @@ def get_image_path(screenshot, filename):
     return os.path.join("screenshots", slugify(screenshot.project.title), filename)
 
 
-class CommonInfo(models.Model):
+class Project(models.Model):
     published = models.BooleanField(default=False, db_index=True)
     pub_date = models.DateTimeField('date published', blank=True, null=True)
     title = models.CharField(max_length=1024)
     slug = models.CharField(max_length=1024, db_index=True, null=False, unique=True)
     lead = models.TextField(blank=True, null=True)
     body = RedactorField(verbose_name="Body", blank=True, null=True)
-
-    def auto_slug(self):
-        return slugify(self.title)
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            if not self.slug:
-                self.slug = self.auto_slug()
-        super(CommonInfo, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        abstract = True
-
-
-class Project(CommonInfo):
     preview = models.OneToOneField('Screenshot', related_name='preview', blank=True, null=True, unique=False)
     project_title = models.CharField(max_length=1024)
     download_url = models.CharField(max_length=1024, blank=True)
@@ -47,6 +29,21 @@ class Project(CommonInfo):
         except IndexError:
             return None
 
+    def auto_slug(self):
+        return slugify(self.title)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            if not self.slug:
+                self.slug = self.auto_slug()
+        super(Project, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        abstract = True
+
 
 class Screenshot(models.Model):
     project = models.ForeignKey(Project)
@@ -54,14 +51,6 @@ class Screenshot(models.Model):
 
     def __str__(self):
         return self.image_path.url
-
-
-class Post(CommonInfo):
-    pass
-
-
-class Screencast(CommonInfo):
-    pass
 
 
 class Settings(models.Model):
