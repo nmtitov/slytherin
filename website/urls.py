6007cf1e14c10b483082e1e99101f271ad736126
Nikita Titov <nmtitov@yandex.ru>
2015-01-26 22:54:15 +0200

    Fix admin url issue

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 
-urlpatterns = patterns(
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('blog.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
