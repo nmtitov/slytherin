8b54d4e818ece1452927129cf94f9f4bcbbf689f
Nikita Titov <nmtitov@yandex.ru>
2015-02-01 15:02:24 +0200

    Fix property access issue

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 
 def posts(request):
     template_name = "blog/posts.html"
-    if request.user.is_staff():
+    if request.user.is_staff:
         xs = Post.objects.filter(Q(published=True) | Q(preview=True)).order_by('-publication_date')
     else:
         xs = Post.objects.filter(published=True).order_by('-publication_date')
@@ -17,7 +17,7 @@ def posts(request):
 def post(request, slug: str=None):
     template_name = 'blog/post.html'
     try:
-        if request.user.is_staff():
+        if request.user.is_staff:
             x = Post.objects.filter(Q(published=True) | Q(preview=True)).get(slug=slug)
         else:
             x = Post.objects.filter(published=True).get(slug=slug)
