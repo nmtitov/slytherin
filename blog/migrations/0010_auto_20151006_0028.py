19fddcc48cf69d38318db987b2d2e4e03fa22768
Nikita Titov <nmtitov@yandex.ru>
2015-10-06 02:29:10 +0200

    title_before, title_after

---
+++ blog/migrations/0010_auto_20151006_0028.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0009_image_caption'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='title_after',
+            field=models.CharField(default='', max_length=1024),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='title_before',
+            field=models.CharField(default='', max_length=1024),
+            preserve_default=False,
+        ),
+    ]
