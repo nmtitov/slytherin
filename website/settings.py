b5761b5eef930b83fc0e9da5fc923ee00ad8bad4
Nikita Titov <nmtitov@yandex.ru>
2016-04-03 20:31:58 +0200

    Update static and media settings

--- website/settings.py
+++ website/settings.py
@@ -89,19 +89,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "staticfiles"),
+)
 
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-if DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-else:
-    MEDIA_ROOT = '/var/www/nmtitov/media/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/nmtitov/static_dir/',
-)
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 TEMPLATES = [
@@ -122,7 +118,5 @@ TEMPLATES = [
         },
     },
 ]
-
-STATIC_ROOT = "/var/www/nmtitov/static_root/"
 
 AXES_LOGIN_FAILURE_LIMIT = 10
