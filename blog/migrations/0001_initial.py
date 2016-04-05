a08867b9c43573bbc609f6712029d452ffbb6e18
Nikita Titov <nmtitov@yandex.ru>
2016-04-06 00:25:05 +0200

    Squash migrations

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -72,6 +72,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=32, unique=True)),
+                ('verbose_title', models.CharField(max_length=1024)),
                 ('slug', models.CharField(max_length=32, unique=True)),
             ],
         ),
