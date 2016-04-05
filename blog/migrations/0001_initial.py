2062ccc8f42de6f7984be56d79b4838b4ff6293f
Nikita Titov <nmtitov@yandex.ru>
2016-04-05 03:13:46 +0200

    squash Section.slug

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -66,6 +66,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=32, unique=True)),
+                ('slug', models.CharField(max_length=32, unique=True)),
             ],
         ),
         migrations.AddField(
