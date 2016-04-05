3ee7ba3eb7ccbcf5f4ea4216786fef321b0e0f55
Nikita Titov <nmtitov@yandex.ru>
2016-04-05 02:11:48 +0200

    Fix queries and braces

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def posts(request, section=None):
         raise Http404
     template_name = "blog/posts.html"
     if request.user.is_staff:
-        xs = Post.objects.filter(Q(section=s) & Q(published=True) | Q(preview=True)).order_by('-publication_date')
+        xs = Post.objects.filter(Q(section=s) & (Q(published=True) | Q(preview=True))).order_by('-publication_date')
     else:
         xs = Post.objects.filter(Q(section=s) & Q(published=True)).order_by('-publication_date')
     settings = Settings.shared_instance()
@@ -24,9 +24,9 @@ def post(request, slug=None):
     template_name = 'blog/post.html'
     try:
         if request.user.is_staff:
-            x = Post.objects.get(slug=slug).filter(Q(published=True) | Q(preview=True))
+            x = Post.objects.get(Q(slug=slug) & (Q(published=True) | Q(preview=True)))
         else:
-            x = Post.objects.get(slug=slug).filter(published=True)
+            x = Post.objects.get(slug=slug, published=True)
     except Post.DoesNotExist:
         raise Http404
     settings = Settings.shared_instance()
