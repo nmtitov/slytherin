6ce4ed5cf202e88701e6de4296b49e5bcde580f9
Nikita Titov <nmtitov@yandex.ru>
2016-04-05 02:10:13 +0200

    Filter by section

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import Http404
-from blog.models import Post, Settings
+from blog.models import Section, Post, Settings
 from django.db.models import Q
 
 
-def posts(request):
+def posts(request, section=None):
+    section = section if section else "Production"
+    try:
+        s = Section.objects.get(title=section)
+    except Section.DoesNotExist:
+        raise Http404
     template_name = "blog/posts.html"
     if request.user.is_staff:
-        xs = Post.objects.filter(Q(published=True) | Q(preview=True)).order_by('-publication_date')
+        xs = Post.objects.filter(Q(section=s) & Q(published=True) | Q(preview=True)).order_by('-publication_date')
     else:
-        xs = Post.objects.filter(published=True).order_by('-publication_date')
+        xs = Post.objects.filter(Q(section=s) & Q(published=True)).order_by('-publication_date')
     settings = Settings.shared_instance()
     context = dict(posts=xs, settings=settings)
     return render(request, template_name, context)
