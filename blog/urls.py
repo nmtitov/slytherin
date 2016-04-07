acd705ce34ef7c06a378c2d62710197108894c5b
Nikita Titov <nmtitov@yandex.ru>
2016-04-07 15:09:27 +0200

    Change /projects url to /production

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.posts, name='posts'),
-    url(r'^projects$', views.posts),
+    url(r'^$', views.posts, name='production'),
+    url(r'^production', views.posts),
     url(r'^tutorials$', views.posts, name='tutorials', kwargs={'section_slug': 'tutorials'}),
-    url(r'^projects/(?P<slug>[A-Za-z0-9_-]+)/$', views.post, name='production_object'),
+    url(r'^production/(?P<slug>[A-Za-z0-9_-]+)/$', views.post, name='production_object'),
     url(r'^tutorials/(?P<slug>[A-Za-z0-9_-]+)/$', views.post, name='tutorials_object', kwargs={'section_slug': 'tutorials'}),
 ]
