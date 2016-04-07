e4637b1651f010c2a0c3e2ef3a582538918c63e0
Nikita Titov <nmtitov@yandex.ru>
2016-04-07 15:25:49 +0200

    Refactor views

--- blog/views.py
+++ blog/views.py
@@ -7,29 +7,26 @@ from django.db.models import Q
 def posts(request, section_slug):
     try:
         section = Section.objects.get(slug=section_slug)
+        if request.user.is_staff:
+            xs = Post.objects.filter(Q(section=section) & (Q(published=True) | Q(preview=True))).order_by('-publication_date')
+        else:
+            xs = Post.objects.filter(Q(section=section) & Q(published=True)).order_by('-publication_date')
+        settings = Settings.shared_instance()
+        context = dict(section=section, posts=xs, settings=settings)
+        return render(request, 'blog/posts.html', context)
     except Section.DoesNotExist:
         raise Http404
-    template_name = "blog/posts.html"
-    if request.user.is_staff:
-        xs = Post.objects.filter(Q(section=section) & (Q(published=True) | Q(preview=True))).order_by('-publication_date')
-    else:
-        xs = Post.objects.filter(Q(section=section) & Q(published=True)).order_by('-publication_date')
-    settings = Settings.shared_instance()
-    context = dict(section=section, posts=xs, settings=settings)
-    return render(request, template_name, context)
 
 
 def post(request, section_slug, slug):
-    section_slug = section_slug if section_slug else Section.DEFAULT_SLUG
-    template_name = 'blog/post.html'
     try:
         section = Section.objects.get(slug=section_slug)
         if request.user.is_staff:
             x = Post.objects.get(Q(slug=slug) & (Q(published=True) | Q(preview=True)))
         else:
             x = Post.objects.get(slug=slug, published=True)
+        settings = Settings.shared_instance()
+        context = dict(section=section, post=x, settings=settings)
+        return render(request, 'blog/post.html', context)
     except (Section.DoesNotExist, Post.DoesNotExist):
         raise Http404
-    settings = Settings.shared_instance()
-    context = dict(section=section, post=x, settings=settings)
-    return render(request, template_name, context)
