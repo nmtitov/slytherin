76e8f4376c2cf1ff11e917fe71fd2963496978f2
Nikita Titov <nmtitov@yandex.ru>
2016-04-07 16:46:23 +0200

    use templategs

---
+++ blog/templatetags/section.py
@@ -0,0 +1,9 @@
+from django import template
+from blog.models import Section
+
+register = template.Library()
+
+
+@register.filter
+def active_if(section: Section, active_slug):
+    return "active" if section.slug == active_slug else None
