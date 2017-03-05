a2521e0599b92eb58c4d5360c59f57c1985af6e8
Nikita Titov <nmtitov@yandex.ru>
2017-03-05 18:51:03 +0200

    Remove "preview"

--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,7 @@ from django.db.models import Q
 def posts(request, section_slug):
     try:
         section = Section.objects.get(slug=section_slug)
-        if request.user.is_staff:
-            xs = Post.objects.filter(Q(section=section) & (Q(published=True) | Q(preview=True))).order_by('-publication_date')
-        else:
-            xs = Post.objects.filter(Q(section=section) & Q(published=True)).order_by('-publication_date')
+        xs = Post.objects.filter(section=section, published=True).order_by('-publication_date')
         settings = Settings.shared_instance()
         context = dict(section=section, posts=xs, settings=settings)
         return render(request, 'blog/posts.html', context)
@@ -21,10 +18,7 @@ def posts(request, section_slug):
 def post(request, section_slug, slug):
     try:
         section = Section.objects.get(slug=section_slug)
-        if request.user.is_staff:
-            x = Post.objects.get(Q(slug=slug) & (Q(published=True) | Q(preview=True)))
-        else:
-            x = Post.objects.get(slug=slug, published=True)
+        x = Post.objects.get(slug=slug, published=True)
         settings = Settings.shared_instance()
         context = dict(section=section, post=x, settings=settings)
         return render(request, 'blog/post.html', context)
