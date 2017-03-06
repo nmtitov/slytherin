9c6e3bc9f9fe976bb6fbb40e1320ba424b2370fe
Nikita Titov <nmtitov@yandex.ru>
2017-03-06 17:55:40 +0200

    Rename publication templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from blog import views
 urlpatterns = [
     # url(r'^$', views.posts, name='posts', kwargs={'section_slug': 'production'}),
     url(r'^$', views.root, name='root'),
-    url(r'^(?P<section>[A-Za-z0-9_-]+)/$', views.posts, name='posts'),
-    url(r'^(?P<section>[A-Za-z0-9_-]+)/(?P<post>[A-Za-z0-9_-]+)/$', views.post, name='post'),
+    url(r'^(?P<section>[A-Za-z0-9_-]+)/$', views.publications, name='publications'),
+    url(r'^(?P<section>[A-Za-z0-9_-]+)/(?P<publication>[A-Za-z0-9_-]+)/$', views.publication, name='publication'),
 
     # url(r'^tutorials$', views.posts, name='tutorials', kwargs={'section_slug': 'tutorials'}),
     # url(r'^tutorials/(?P<slug>[A-Za-z0-9_-]+)/$', views.post, name='tutorials_object', kwargs={'section_slug': 'tutorials'}),
--- blog/views.py
+++ blog/views.py
@@ -10,33 +10,33 @@ def root(request):
         s = Section.get_root()
         p = Publication.list_by_section(s)
         settings = Settings.get()
-        context = dict(sections=sections, section=s, root=r, posts=p, settings=settings)
-        return render(request, 'blog/posts.html', context)
+        context = dict(sections=sections, section=s, root=r, publications=p, settings=settings)
+        return render(request, 'blog/publications.html', context)
     except Section.DoesNotExist:
         raise Http404
 
 
-def posts(request, section: str):
+def publications(request, section: str):
     try:
         r = Section.get_root()
         sections = Section.list()
         s = Section.get_by_slug(section)
         p = Publication.list_by_section(s)
         settings = Settings.get()
-        context = dict(sections=sections, section=s, root=r, posts=p, settings=settings)
-        return render(request, 'blog/posts.html', context)
+        context = dict(sections=sections, section=s, root=r, publications=p, settings=settings)
+        return render(request, 'blog/publications.html', context)
     except Section.DoesNotExist:
         raise Http404
 
 
-def post(request, section, post):
+def publication(request, section, publication):
     try:
         r = Section.get_root()
         sections = Section.list()
         s = Section.get_by_slug(section)
-        p = Publication.get_by_section_and_slug(section=s, slug=post)
+        p = Publication.get_by_section_and_slug(section=s, slug=publication)
         settings = Settings.get()
-        context = dict(root=r, sections=sections, section=s, post=p, settings=settings)
-        return render(request, 'blog/post.html', context)
+        context = dict(root=r, sections=sections, section=s, publication=p, settings=settings)
+        return render(request, 'blog/publication.html', context)
     except (Section.DoesNotExist, Publication.DoesNotExist):
         raise Http404
