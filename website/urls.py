671cbe8f85973e2f2c3479b4b06d708ab2acb79c
Nikita Titov <nmtitov@yandex.ru>
2017-03-06 18:11:58 +0200

    Update site details

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 
+admin.site.site_header = 'Sitename'
+
+# Text to put at the end of each page's <title>.
+admin.site.site_title = 'Sitename site admin'
+
+# Text to put in each page's <h1>.
+admin.site.site_header = 'Sitename'
+
+# Text to put at the top of the admin index page.
+admin.site.index_title = 'Sitename administration'
+
 urlpatterns = [
     url(r'^s/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
