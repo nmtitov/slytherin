b2ad36d03708173059df1d32c3a84341753fe476
Nikita Titov <nmtitov@yandex.ru>
2017-03-06 16:17:59 +0200

    Return empty string instead of none

--- blog/templatetags/section.py
+++ blog/templatetags/section.py
@@ -6,4 +6,4 @@ register = template.Library()
 
 @register.filter
 def active_if(section: Section, active_slug):
-    return "active" if section.slug == active_slug else None
+    return "active" if section.slug == active_slug else ""
