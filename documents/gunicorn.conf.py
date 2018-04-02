db477b28b6191ca0a43a6d52e8001b241c193498
Nikita Titov <nmtitov@yandex.ru>
2018-04-02 17:14:41 +0200

    Copy gunicorn.conf to documents

---
+++ documents/gunicorn.conf.py
@@ -0,0 +1,7 @@
+bind = "unix:/home/nt/nmtitov/gunicorn.socket"
+workers = 2
+logfile = "/home/nt/nmtitov/gunicorn.log"
+loglevel = "info"
+proc_name = "stats"
+pidfile = "/home/nt/nmtitov/gunicorn.pid"
+pythonpath = "/home/nt/nmtitov/"
