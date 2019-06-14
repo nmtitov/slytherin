281a3c6d7c9e2efe312ab5b5fec40a42049438e3
Nikita Titov <nmtitov@ya.ru>
2019-06-15 00:40:14 +0200

    Extract DATABASES

--- project/settings.py
+++ project/settings.py
@@ -87,12 +87,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = local_settings.DATABASES
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
