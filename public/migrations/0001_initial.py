ed036c90bcd5e78a559199e75e35e39e4ffa7ebe
Nikita Titov <nmtitov@ya.ru>
2019-06-16 10:50:07 +0200

    Squash migrations

--- public/migrations/0001_initial.py
+++ public/migrations/0001_initial.py
@@ -40,6 +40,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=256)),
+                ('internal_title', models.CharField(max_length=256)),
                 ('slug', models.SlugField(max_length=256, unique=True)),
                 ('thumbnail', models.TextField(blank=True)),
                 ('body', models.TextField(blank=True)),
