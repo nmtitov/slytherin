0562653a24c41906134055932952fb31a4fdab7e
Nikita Titov <nmtitov@ya.ru>
2019-06-19 10:35:06 +0200

    Group posts by year

--- public/views.py
+++ public/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from django.shortcuts import render
 from django.http import Http404
 from .models import Post, Section, Settings
@@ -5,11 +6,23 @@ from .models import Post, Section, Settings
 
 def posts(request, section=None):
     try:
-        s = Section.get_by_slug(section) if section else Section.get_home()
+        # Get model objects
+        section_object = Section.get_by_slug(section) if section else Section.get_home()
+        post_objects = Post.list_by_section(section=section_object)
+
+        # Group posts by year
+        posts_default_dict = defaultdict(list)
+        for post_object in post_objects:
+            key = post_object.release_date.year if post_object.release_date.year else 0
+            posts_default_dict[key].append(post_object)
+        posts_dict = dict(posts_default_dict)
+        print(posts_dict)
+
+        # Render template
         context = {
             "sections": Section.list(),
-            "section": s,
-            "posts": Post.list_by_section(s),
+            "section": section_object,
+            "posts_dict": posts_dict,
             "settings": Settings.get(),
         }
         return render(request, 'public/posts.html', context)
