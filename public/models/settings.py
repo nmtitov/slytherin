7a13004453318a71c3f524eedf65835740c2339e
Nikita Titov <nmtitov@ya.ru>
2019-06-19 12:23:42 +0200

    Merge Settings.blog_copyright, Settings.author_email, Settings.counter_yandex_metrika -> Settings.footer

--- public/models/settings.py
+++ public/models/settings.py
@@ -3,9 +3,7 @@ from django.db import models
 
 class Settings(models.Model):
     blog_title = models.CharField(max_length=1024)
-    blog_copyright = models.CharField(max_length=1024)
-    author_email = models.EmailField(max_length=254)
-    counter_yandex_metrika = models.TextField(blank=True, null=True)
+    footer = models.TextField(blank=True, null=True)
 
     @classmethod
     def get(cls):
