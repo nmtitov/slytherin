8579f49da82646337c56ab08b62ec71486dbd7ac
Nikita Titov <nmtitov@ya.ru>
2019-06-23 21:51:20 +0200

    Delete dead code

--- public/admin.py
+++ public/admin.py
@@ -7,14 +7,8 @@ class SectionAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# class ImageInline(admin.StackedInline):
-#     model = Image
-#     extra = 4
-
-
 @admin.register(Post)
 class PublicationAdmin(admin.ModelAdmin):
-    # inlines = [ImageInline]
     list_display = ('title', 'section', 'created_date', 'release_date', )
     list_filter = ('section', 'created_date', 'release_date', )
 
