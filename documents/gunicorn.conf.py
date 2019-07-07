a4703124302ee85ba4fd2596d3b60dcbc1dcc200
Nikita Titov <nmtitov@ya.ru>
2019-07-07 12:13:23 +0200

    Update gunicorn template file

--- documents/gunicorn.conf.py
+++ documents/gunicorn.conf.py
@@ -2,6 +2,6 @@ bind = "unix:/tmp/titov.sock"
 workers = 2
 logfile = "/var/log/titov/gunicorn.log"
 loglevel = "info"
-proc_name = "inv"
+proc_name = "titov"
 pidfile = "/var/run/titov/gunicorn.pid"
 pythonpath = "/home/nt/titov/"
